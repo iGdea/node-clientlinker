07b9ee45154973f175175824ce9d55df92b87dc9
Bacra Woo <bacra.woo@gmail.com>
2016-08-28 01:49:52 +0800

    Upate: logger assert

--- test/test_flows.js
+++ test/test_flows.js
@@ -142,49 +142,137 @@ describe('#flows', function()
 	describe('#logger', function()
 	{
 		var logger = require('../flows/logger/logger');
-		it('#run', function(done)
+		function PromiseDeffer()
 		{
-			var linker = ClientLinker(
+			var resolve, reject;
+			var promise = new Promise(function(resolve0, reject0)
+			{
+				resolve = resolve0;
+				reject = reject0;
+			});
+
+			return {
+				promise: promise,
+				resolve: resolve,
+				reject: reject
+			};
+		}
+
+		function simpleLinker(genLoggerHander)
+		{
+			return ClientLinker(
 				{
-					flows: ['logger', 'custom'],
-					defaults: {
-						logger: function(runtime, err, data)
-						{
-							var timing = runtime.timing;
-							var lastFlowTiming = runtime.lastFlow().timing;
-
-							logger.loggerHandler.apply(null, arguments);
-
-							try {
-								expect(lastFlowTiming.start).to.be.an('number');
-								expect(lastFlowTiming.end).to.be.an('number');
-								expect(timing.flowsStart).to.be.an('number');
-								expect(timing.flowsEnd).to.be.an('number');
-								expect(err).to.be(null);
-								expect(data.respone).to.be('respone');
-
-								done();
-							}
-							catch(err)
-							{
-								done(err);
-							}
-						},
-					},
-					customFlows:
-					{
-						custom: function custom(runtime, callback)
-						{
-							callback(null, {respone: 'respone'});
-						}
-					},
+					flows: ['logger', 'confighandler'],
 					clients:
 					{
-						client: null
+						client_run:
+						{
+							logger: genLoggerHander('run'),
+							confighandler:
+							{
+								method: function()
+								{
+									return Promise.resolve('success');
+								}
+							}
+						},
+						client_error:
+						{
+							logger: genLoggerHander('error'),
+							confighandler:
+							{
+								method: function()
+								{
+									return Promise.reject('error');
+								}
+							}
+						}
 					}
 				});
+		}
 
-			linker.run('client.method', 123, {body:456}).catch(done);
+		it('#param', function()
+		{
+			var runDeffer = PromiseDeffer();
+			var errorDeffer = PromiseDeffer();
+			var linker = simpleLinker(function(type)
+			{
+				return function(runtime, err, data)
+				{
+					try {
+						var timing = runtime.timing;
+						var lastFlowTiming = runtime.lastFlow().timing;
+
+						expect(lastFlowTiming.start).to.be.an('number');
+						expect(lastFlowTiming.end).to.be.an('number');
+						expect(timing.flowsStart).to.be.an('number');
+						expect(timing.flowsEnd).to.be.an('number');
+
+						if (type == 'error')
+						{
+							expect(err).to.be('error');
+							expect(data).to.be(null);
+							errorDeffer.resolve();
+						}
+						else
+						{
+							expect(err).to.be(null);
+							expect(data).to.be('success');
+							runDeffer.resolve();
+						}
+					}
+					catch(err)
+					{
+						type == 'error'
+							? errorDeffer.reject(err)
+							: runDeffer.reject(err);
+					}
+				};
+			});
+
+			return Promise.all(
+			[
+				linker.run('client_run.method'),
+				linker.run('client_error.method')
+					.then(function(){expect().fail()}, function(){}),
+				runDeffer.promise,
+				errorDeffer.promise
+			]);
+		});
+
+		it('#defaultLoggerHander', function()
+		{
+			var runDeffer = PromiseDeffer();
+			var errorDeffer = PromiseDeffer();
+			var linker = simpleLinker(function(type)
+			{
+				return function()
+				{
+					try {
+						logger.loggerHandler.apply(null, arguments);
+						if (type == 'error')
+							errorDeffer.resolve();
+						else
+							runDeffer.resolve();
+					}
+					catch(err)
+					{
+						if (type == 'error')
+							errorDeffer.reject(err);
+						else
+							runDeffer.reject(err);
+					}
+				};
+			});
+
+			return Promise.all(
+			[
+				linker.run('client_run.method'),
+				linker.run('client_error.method')
+					.then(function(){expect().fail()}, function(){}),
+				runDeffer.promise,
+				errorDeffer.promise
+			]);
 		});
 	});
 
