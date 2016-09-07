eb1ba2fb71be1edd694681c7bc6b1f900f9eec18
Bacra Woo <bacra.woo@gmail.com>
2016-09-08 01:57:21 +0800

    Update: add `runAction` for route

--- flows/httpproxy/route.js
+++ flows/httpproxy/route.js
@@ -10,96 +10,113 @@ exports = module.exports = HttpProxyRoute;
 function HttpProxyRoute(linker)
 {
 	if (!linker) return function(req, res, next){next()};
-
 	linker.__bind_httpproxy_route__ = true;
-
-	function sendStatus(res, code, env)
-	{
-		res.statusCode = code;
-		res.json(linker.JSON.stringify(
-			{
-				env: env,
-				CONST_VARS: linker.JSON.CONST_VARS
-			}));
-	}
 
 	return function HttpProxyRouteHandle(req, res, next)
 	{
 		var action = req.query.action;
 		if (!action) return next();
 
+		function sendStatus(res, code, env)
+		{
+			res.statusCode = code;
+			res.json(linker.JSON.stringify(
+				{
+					env: env,
+					CONST_VARS: linker.JSON.CONST_VARS
+				}));
+		}
+
 		rawBody(req).then(function(buf)
 			{
 				var body = JSON.parse(buf.toString());
+				if (body.CONST_VARS)
+					body = linker.JSON.parse(body, body.CONST_VARS);
 
-				return linker.parseAction(action)
-					.then(function(methodInfo)
-					{
-						if (body.CONST_VARS)
-							body = linker.JSON.parse(body, body.CONST_VARS);
-
-						var options = methodInfo.client && methodInfo.client.options;
-
-						if (checkHttpproxyKey(action, body, options) ===  false)
-							return sendStatus(res, 403, body.env);
-
-						debug('[%s] catch proxy route', action);
-						var args = [action, body.query, body.body, null, body.options];
-						var retPromise = linker.runIn(args, 'httpproxy', body.env);
-						retPromise.then(function(data)
-							{
-								var runtime = retPromise.runtime;
-								// console.log('svr env for data', runtime.env);
-								var json = linker.JSON.stringify(
-									{
-										env: runtime.env,
-										data: data,
-										CONST_VARS: linker.JSON.CONST_VARS
-									});
-
-								res.json(json);
-							})
-							.catch(function(err)
-							{
-								var runtime = retPromise.runtime;
-								var env = runtime ? runtime.env : body.env;
-								// console.log('svr env for err', env);
-								if (err
-									&& (err.CLIENTLINKER_TYPE == 'CLIENT FLOW OUT'
-										|| err.CLIENTLINKER_TYPE == 'CLIENT NO FLOWS'
-										|| err.CLIENTLINKER_TYPE == 'NO CLIENT'))
-								{
-									debug('[%s] %s', action, err);
-									return sendStatus(res, 501, env);
-								}
-								else
-								{
-									var json = linker.JSON.stringify(
-										{
-											env: env,
-											result: err,
-											CONST_VARS: linker.JSON.CONST_VARS
-										});
-
-									res.json(json);
-								}
-							});
-					})
+				return runAction(linker, action, body)
 					.catch(function(err)
 					{
 						debug('[%s] linker run err:%o', action, err);
-						sendStatus(res, 500, body && body.env);
-					});
+						return {
+							statusCode: 500,
+							env: body && body.env,
+							result: err
+						};
+					})
+					.then(function(data)
+					{
+						res.statusCode = data.statusCode;
+						var output = {
+							env: data.env,
+							result: data.result,
+							data: data.data
+						};
 
-		})
-		.catch(function()
-		{
-			debug('[%s] parse body err:%o', action, err);
-			sendStatus(res, 500);
-		});
+						output.CONST_VARS = linker.JSON.CONST_VARS;
+						output = linker.JSON.stringify(output);
+						res.json(output);
+					});
+			})
+			.catch(function()
+			{
+				debug('[%s] parse body err:%o', action, err);
+				res.statusCode = 500;
+				res.json({});
+			});
 	};
 }
 
+
+function runAction(linker, action, body)
+{
+	return linker.parseAction(action)
+		.then(function(methodInfo)
+		{
+			var options = methodInfo.client && methodInfo.client.options;
+
+			if (checkHttpproxyKey(action, body, options) ===  false)
+				return {statusCode: 403};
+
+			debug('[%s] catch proxy route', action);
+			var args = [action, body.query, body.body, null, body.options];
+			var retPromise = linker.runIn(args, 'httpproxy', body.env);
+
+			return retPromise.then(function(data)
+				{
+					var runtime = retPromise.runtime;
+					// console.log('svr env for data', runtime.env);
+					return {
+						statusCode: 200,
+						env: runtime.env,
+						data: data
+					};
+				})
+				.catch(function(err)
+				{
+					var runtime = retPromise.runtime;
+					var output = {
+						env: runtime ? runtime.env : body.env,
+						result: err
+					};
+
+					// console.log('svr env for err', env);
+					if (err
+						&& (err.CLIENTLINKER_TYPE == 'CLIENT FLOW OUT'
+							|| err.CLIENTLINKER_TYPE == 'CLIENT NO FLOWS'
+							|| err.CLIENTLINKER_TYPE == 'NO CLIENT'))
+					{
+						debug('[%s] %s', action, err);
+						output.statusCode = 501;
+					}
+					else
+					{
+						output.statusCode = 200;
+					}
+
+					return output;
+				});
+		});
+}
 
 
 exports.checkHttpproxyKey = checkHttpproxyKey;
