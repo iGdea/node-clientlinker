de8dcbb099d420fcbd424aba2cf5cd58095a46cc
Bacra Woo <bacra.woo@gmail.com>
2016-09-19 03:09:57 +0800

    Add: `process.env.LOG_VERBOSE` for cli init

--- bin/clientlinker_cli.js
+++ bin/clientlinker_cli.js
@@ -5,6 +5,6 @@ process.title = 'clientlinker';
 process.env.CLIENTLINKER_CLI = true;
 
 // hook console
-require('./lib/stdout').is_verbose = false;
+require('./lib/stdout').is_verbose = process.env.LOG_VERBOSE == 'true';
 // start command
 require('./main');
