24aba60ed1cb01892282c56381bed644fe2f19ca
Bacra Woo <bacra.woo@gmail.com>
2018-05-07 02:31:34 +0800

    Add: native vs bluebird

--- benchmark/promise.js
+++ benchmark/promise.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var Promise = require('bluebird');
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
@@ -9,58 +8,6 @@ suite.add('#directly', function()
 	{
 		'1,2,3'.split(',')
 	})
-
-	.add('#promise1', function(deferred)
-	{
-		new Promise(function(resolve)
-			{
-				'1,2,3'.split(',')
-				resolve();
-			})
-			.then(function(data){deferred.resolve(data)},
-				function(err){deferred.reject(err)});
-	},
-	{'defer': true})
-
-	.add('#promise2', function(deferred)
-	{
-		Promise.resolve()
-			.then(function()
-			{
-				'1,2,3'.split(',')
-			})
-			.then(function(data){deferred.resolve(data)},
-				function(err){deferred.reject(err)});
-	},
-	{'defer': true})
-
-	.add('#promise3', function(deferred)
-	{
-		Promise.resolve()
-			.then(function(){return Promise.resolve()})
-			.then(function()
-			{
-				'1,2,3'.split(',')
-			})
-			.then(function(data){deferred.resolve(data)},
-				function(err){deferred.reject(err)});
-	},
-	{'defer': true})
-
-	.add('#promise4', function(deferred)
-	{
-		Promise.resolve()
-			.then(function(){return Promise.resolve()})
-			.then(function(){return Promise.resolve()})
-			.then(function()
-			{
-				'1,2,3'.split(',')
-			})
-			.then(function(data){deferred.resolve(data)},
-				function(err){deferred.reject(err)});
-	},
-	{'defer': true})
-
 	.on('cycle', function(event)
 	{
 		console.log(String(event.target));
@@ -68,5 +15,63 @@ suite.add('#directly', function()
 	.on('complete', function()
 	{
 		console.log('Fastest is ' + this.filter('fastest').map('name'));
-	})
-	.run({ 'async': true });
+	});
+
+addPromiseCases('native', Promise);
+addPromiseCases('bluebird', require('bluebird'));
+
+suite.run({ 'async': true });
+
+function addPromiseCases(name, Promise)
+{
+	suite.add('#'+name+' promise1', function(deferred)
+		{
+			new Promise(function(resolve)
+				{
+					'1,2,3'.split(',')
+					resolve();
+				})
+				.then(function(data){deferred.resolve(data)},
+					function(err){deferred.reject(err)});
+		},
+		{'defer': true})
+
+		.add('#'+name+' promise2', function(deferred)
+		{
+			Promise.resolve()
+				.then(function()
+				{
+					'1,2,3'.split(',')
+				})
+				.then(function(data){deferred.resolve(data)},
+					function(err){deferred.reject(err)});
+		},
+		{'defer': true})
+
+		.add('#'+name+' promise3', function(deferred)
+		{
+			Promise.resolve()
+				.then(function(){return Promise.resolve()})
+				.then(function()
+				{
+					'1,2,3'.split(',')
+				})
+				.then(function(data){deferred.resolve(data)},
+					function(err){deferred.reject(err)});
+		},
+		{'defer': true})
+
+		.add('#'+name+' promise4', function(deferred)
+		{
+			Promise.resolve()
+				.then(function(){return Promise.resolve()})
+				.then(function(){return Promise.resolve()})
+				.then(function()
+				{
+					'1,2,3'.split(',')
+				})
+				.then(function(data){deferred.resolve(data)},
+					function(err){deferred.reject(err)});
+		},
+		{'defer': true});
+}
