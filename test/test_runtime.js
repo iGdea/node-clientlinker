51fda2539413ce4b012f172ff43165546250f7d3
Bacra Woo <bacra.woo@gmail.com>
2018-09-12 12:54:29 +0800

    Add: toJSON for runtime

--- test/test_runtime.js
+++ test/test_runtime.js
@@ -102,4 +102,111 @@ describe('#runtime', function()
 		});
 	});
 
+
+	it('#debug', function()
+	{
+		var linker = clientlinker(
+		{
+			flows: ['custom1'],
+			customFlows:
+			{
+				custom1: function(runtime, callback)
+				{
+					runtime.debug('key1', 21);
+					callback.resolve();
+				},
+			},
+			clients:
+			{
+				client: null
+			}
+		});
+
+		var promise = linker.run('client.method');
+		var runtime = linker.lastRuntime;
+
+		return promise.then(function()
+		{
+			expect(runtime.debug('key1')).to.be(21);
+			var alldata = runtime.debug();
+			expect(Object.keys(alldata)).to.eql(['key1']);
+			expect(alldata.key1).to.be.an('array');
+			expect(alldata.key1.length).to.be(1);
+			expect(alldata.key1[0].toJSON()).to.eql({retry: 1, value: 21, flow: 'custom1'});
+		});
+	});
+
+	it('#toJSON', function()
+	{
+		var linker = clientlinker(
+		{
+			flows: ['custom1', 'custom2'],
+			customFlows:
+			{
+				custom1: function(runtime, callback)
+				{
+					return callback.next();
+				},
+				custom2: function(runtime, callback)
+				{
+					callback.resolve();
+				}
+			},
+			clients:
+			{
+				client: null
+			}
+		});
+
+		var promise = linker.run('client.method', 'query', 'body', 'run_options');
+		var runtime = linker.lastRuntime;
+
+		return promise.then(function()
+		{
+			var data = runtime.toJSON();
+			expect(data.navigationStart).to.be.a('number');
+			delete data.navigationStart;
+
+			var timing = data.timing;
+			delete data.timing;
+			expect(Object.keys(timing)).to.eql(['startTime', 'endTime']);
+			expect(timing.startTime).to.be.a('number');
+			expect(timing.endTime).to.be.a('number');
+
+			var retry = data.retry;
+			delete data.retry;
+			expect(retry).to.be.an('array');
+			expect(retry.length).to.be(1);
+
+			timing = retry[0].timing;
+			delete retry[0].timing;
+			expect(Object.keys(timing)).to.eql(['startTime', 'endTime']);
+			expect(timing.startTime).to.be.a('number');
+			expect(timing.endTime).to.be.a('number');
+
+			var runned = retry[0].runned;
+			delete retry[0].runned;
+			expect(runned).to.be.an('array');
+			expect(runned.length).to.be(2);
+			expect(runned[0].startTime).to.be.a('number');
+			expect(runned[0].endTime).to.be.a('number');
+			expect(runned.map(function(item){return item.name}))
+				.to.be.eql(['custom1', 'custom2']);
+
+
+			expect(retry[0]).to.be.eql({started: true, finished: true});
+
+			expect(data).to.be.eql({ action: 'client.method',
+				query: 'query',
+				body: 'body',
+				options: 'run_options',
+				client: 'client',
+				env: { source: 'run' },
+				debugData: {},
+				started: true,
+				finished: true
+			});
+		});
+	});
+
 });
