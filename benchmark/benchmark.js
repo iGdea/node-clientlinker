aeeb21cd86ed463bfc8968d33181361ec4e781d5
Bacra <bacra.woo@gmail.com>
2019-06-12 21:28:04 +0800

    Clear: benchmark for runtime

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -4,7 +4,6 @@ var Promise = require('bluebird');
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 var clientlinker = require('../');
-var Runtime = require('../lib/runtime').Runtime;
 
 function methodHandler(query, body, callback)
 {
@@ -29,50 +28,30 @@ var linker = clientlinker(
 
 linker.flow('confighandler', require('clientlinker-flow-confighandler'));
 
-linker._newRuntime('client.method')
-	.then(function(runtime)
+
+suite.add('#only one promise run', function(deferred)
 	{
-		suite.add('#only one promise run', function(deferred)
+		new Promise(function(resolve)
 			{
-				new Promise(function(resolve)
-					{
-						methodHandler(function(){'2,3'.split(',')}, null, resolve);
-					})
-					.then(deferred.resolve.bind(deferred));
-			},
-			{defer: true})
-
-			.add('#clientlinker', function(deferred)
-			{
-				linker.run('client.method', function(){'2,3'.split(',')})
-					.then(deferred.resolve.bind(deferred));
-			},
-			{defer: true})
-
-			.add('#step runbyruntime', function(deferred)
-			{
-				var runtime2 = new Runtime(runtime.client, runtime.action,
-					runtime.method, function(){'2,3'.split(',')}, {}, {});
-
-				linker._runByRuntime(runtime2)
-					.then(deferred.resolve.bind(deferred));
-			},
-			{defer: true})
-
-			.add('#step newruntime', function(deferred)
-			{
-				linker._newRuntime('client.method', function(){'2,3'.split(',')})
-					.then(deferred.resolve.bind(deferred));
-			},
-			{defer: true})
-
-			.on('cycle', function(event)
-			{
-				console.log(String(event.target));
+				methodHandler(function(){'2,3'.split(',')}, null, resolve);
 			})
-			.on('complete', function()
-			{
-				console.log('Fastest is ' + this.filter('fastest').map('name'));
-			})
-			.run({async: true});
-	});
+			.then(deferred.resolve.bind(deferred));
+	},
+	{defer: true})
+
+	.add('#clientlinker', function(deferred)
+	{
+		linker.run('client.method', function(){'2,3'.split(',')})
+			.then(deferred.resolve.bind(deferred));
+	},
+	{defer: true})
+
+	.on('cycle', function(event)
+	{
+		console.log(String(event.target));
+	})
+	.on('complete', function()
+	{
+		console.log('Fastest is ' + this.filter('fastest').map('name'));
+	})
+	.run({async: true});
