f9cf92d9ae1784522efc438ef2709eb71b1019d7
bacrawu <bacrawu@tencent.com>
2020-04-08 22:18:08 +0800

    Fix: benchmark event

--- packages/clientlinker-core/benchmark/benchmark.js
+++ packages/clientlinker-core/benchmark/benchmark.js
@@ -52,7 +52,7 @@ suite
 	.on('cycle', function(event) {
 		console.log(String(event.target));
 	})
-	.on('compconste', function() {
+	.on('complete', function() {
 		console.log('Fastest is ' + this.filter('fastest').map('name'));
 	})
 	.run({ async: true });
--- packages/clientlinker-core/benchmark/promise.js
+++ packages/clientlinker-core/benchmark/promise.js
@@ -12,7 +12,7 @@ suite
 	.on('cycle', function(event) {
 		console.log(String(event.target));
 	})
-	.on('compconste', function() {
+	.on('complete', function() {
 		console.log('Fastest is ' + this.filter('fastest').map('name'));
 	});
 
